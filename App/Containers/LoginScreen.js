7850765c2601d0ec78de0eab35ec1c5c6ddef7f3
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-07-23 01:11:45 +0300

    created screen: 'LoginScreen'

--- App/Containers/LoginScreen.js
+++ App/Containers/LoginScreen.js
@@ -1,22 +1,73 @@
 // Packages
 import React, { Component } from "react"
-import { View, Text } from "react-native"
+import {
+    View,
+    Text,
+    SafeAreaView
+} from "react-native"
 import { connect } from "react-redux";
 
 // Actions
 import { AuthActions } from "../Redux/AuthRedux"
 
 // Components
+import Button from "../Components/Button"
 
 // Styles
-import Styles from "./Styles/LoginStyles"
+import styles from "./Styles/LoginStyles"
+import { Colors } from '../Themes'
 
 class LoginScreen extends Component {
 
+    onLoginPress = () => {
+        this.props.navigation.navigate("Incomplete")
+    }
+
     render() {
         return (
-            <View style={Styles.container}>
-                <Text>LoginScreen</Text>
+            <View style={styles.container}>
+                <SafeAreaView style={styles.topContainer}>
+                    <View style={styles.welcomeContainer}>
+                        <Text style={styles.bemagineText}>Bemagine</Text>
+                        <Text style={styles.subText}>Where imagination meets hardwork</Text>
+                    </View>
+                    <View style={styles.loginButtonsContainer}>
+                        <View style={styles.loginButtonContainer}>
+                            <Button
+                                text="Login with Google"
+                                textColor={Colors.lightTextColor}
+                                onPress={this.onLoginPress}
+                                backgroundColor={Colors.googleColor}
+                            />
+                        </View>
+                        <View style={styles.loginButtonContainer}>
+                            <Button
+                                text="Login with Email"
+                                textColor={Colors.lightTextColor}
+                                onPress={this.onLoginPress}
+                                backgroundColor={Colors.brandColor}
+                            />
+                        </View>
+                    </View>
+                </SafeAreaView>
+                <SafeAreaView style={styles.bottomContainer}>
+                    <View style={styles.signupButtonContainer}>
+                        <Button
+                            text="Sign up"
+                            textColor={Colors.darkTextColor}
+                            onPress={this.onLoginPress}
+                            backgroundColor={Colors.lightBackground}
+                        />
+                    </View>
+                    <View style={styles.forgotPassContainer}>
+                        <Button
+                            text="Forgot Password?"
+                            textColor={Colors.midGrey}
+                            onPress={this.onLoginPress}
+                            backgroundColor={"transparent"}
+                        />
+                    </View>
+                </SafeAreaView>
             </View>
         )
     }
