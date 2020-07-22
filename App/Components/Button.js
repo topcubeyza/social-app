653c3853026e8cce19081a3454740bc00bda32b2
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-07-23 01:10:17 +0300

    added component: 'Button'

---
+++ App/Components/Button.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { TouchableOpacity, Text } from "react-native"
+import PropTypes from "prop-types"
+
+import styles from "./Styles/ButtonStyles"
+
+
+const Button = (props) => {
+    console.log(props)
+    debugger;
+    let containerStyle = [
+        styles.container, 
+        { backgroundColor: props.backgroundColor }];
+    let textStyle = [
+        styles.text, 
+        { color: props.textColor }];
+    return (
+        <TouchableOpacity
+            style={containerStyle}
+            onPress={props.onPress}>
+            <Text style={textStyle}>{props.text}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default Button
+
+Button.propTypes = {
+    backgroundColor: PropTypes.string.isRequired,
+    textColor: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired
+}
---
+++ App/Components/Styles/ButtonStyles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+import { Colors, Metrics, Fonts } from '../../Themes/'
+
+export default StyleSheet.create({
+    container: {
+        height: Metrics.buttonHeight,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: Metrics.borderRadiusStandard,
+    },
+    text: {
+        fontFamily: Fonts.type.bold,
+        fontSize: Fonts.size.fourteen,
+    }
+})
