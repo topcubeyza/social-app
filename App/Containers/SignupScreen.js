7fdb7e855b615368a84d396802341b998f34dc6b
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-07-23 10:37:14 +0300

    designed signup screen

--- App/Containers/SignupScreen.js
+++ App/Containers/SignupScreen.js
@@ -1,26 +1,107 @@
 // Packages
 import React, { Component } from "react"
-import { View, Text } from "react-native"
+import {
+    View,
+    Text,
+    SafeAreaView
+} from "react-native"
 import { connect } from "react-redux";
+import _ from "lodash"
 
 // Actions
 import { AuthActions } from "../Redux/AuthRedux"
 
 // Components
+import Button from "../Components/Button"
 
 // Styles
-import Styles from "./Styles/SignupStyles"
+import styles from "./Styles/SignupStyles"
+import { Colors } from '../Themes'
 
-class LoginScreen extends Component {
+class SignupScreen extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            passwordConfirm: ""
+        }
+    }
+
+    onLoginPress = () => {
+        this.props.navigation.navigate("Incomplete")
+    }
+
+    onLoginInsteadPress = () => {
+        this.props.navigation.navigate("Login");
+    }
+
+    isSignupButtonDisabled = () => {
+        let {email, password, passwordConfirm} = this.state
+        if (_.isEmpty(email) || _.isEmpty(password) || _.isEmpty(passwordConfirm)) {
+            console.log("empty input")
+            return true;
+        }
+        if (_.isEqual(password, passwordConfirm)) {
+            return true;
+        }
+
+        return false;
+    }
 
     render() {
+        let signupButtonDisabled = this.isSignupButtonDisabled()
         return (
-            <View style={Styles.container}>
-                <Text>SignupScreen</Text>
+            <View style={styles.container}>
+                <SafeAreaView style={styles.topContainer}>
+                    <View style={styles.welcomeContainer}>
+                        <Text style={styles.bemagineText}>Bemagine</Text>
+                        <Text style={styles.subText}>Where imagination meets hardwork</Text>
+                    </View>
+                    <View style={styles.loginButtonsContainer}>
+                        <View style={styles.loginButtonContainer}>
+                            <Button
+                                text="Login with Google"
+                                textColor={Colors.lightTextColor}
+                                onPress={this.onLoginPress}
+                                backgroundColor={Colors.googleColor}
+                            />
+                        </View>
+                        <View style={styles.loginButtonContainer}>
+                            <Button
+                                text="Login with Email"
+                                textColor={Colors.lightTextColor}
+                                onPress={this.onLoginPress}
+                                backgroundColor={Colors.brandColor}
+                            />
+                        </View>
+                    </View>
+                </SafeAreaView>
+                <SafeAreaView style={styles.bottomContainer}>
+                    <View style={styles.signupButtonContainer}>
+                        <Button
+                            disable={signupButtonDisabled}
+                            text="Sign up"
+                            textColor={Colors.lightTextColor}
+                            onPress={this.onLoginPress}
+                            backgroundColor={signupButtonDisabled ? Colors.darkTextColor : Colors.brandColor}
+                        />
+                    </View>
+                    <View style={styles.forgotPassContainer}>
+                        <Button
+                            text="Login instead?"
+                            textColor={Colors.midGrey}
+                            onPress={this.onLoginInsteadPress}
+                            backgroundColor={"transparent"}
+                        />
+                    </View>
+                </SafeAreaView>
             </View>
         )
     }
 
 }
 
-export default LoginScreen;
+export default SignupScreen;
--- App/Containers/Styles/SignupStyles.js
+++ App/Containers/Styles/SignupStyles.js
@@ -1,9 +1,64 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 import { Colors, Metrics, Fonts } from '../../Themes/'
 
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "orange",
+        justifyContent: "space-between"
     },
+    topContainer: {
+        backgroundColor: Colors.lightBackground,
+        flexGrow: 1
+    },
+    bottomContainer: {
+        backgroundColor: Colors.lightGrey,
+    },
+    signupButtonContainer: {
+        paddingTop: Metrics.width * 0.06,
+        paddingHorizontal: Metrics.marginHorizontalLarge
+    },
+    forgotPassContainer: {
+        alignItems: "center",
+        paddingBottom: Metrics.width * 0.04
+    },
+    forgotPassText: {
+        fontFamily: Fonts.type.bold,
+        fontSize: Fonts.size.fourteen,
+        color: Colors.midGrey,
+    },
+    loginButtonsContainer: {
+        paddingVertical: Metrics.width * 0.04,
+    },
+    loginButtonContainer: {
+        paddingVertical: Metrics.width * 0.02,
+        paddingHorizontal: Metrics.marginHorizontalLarge
+    },
+    welcomeContainer: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: Metrics.marginHorizontalLarge
+    },
+    welcomeTextsContainer: {
+        alignItems: "center",
+        paddingVertical: Metrics.marginVertical,
+        borderRadius: Metrics.borderRadiusStandard,
+    },
+    welcomeText: {
+        fontFamily: Fonts.type.brand,
+        fontSize: Fonts.size.twenty * 2,
+        marginBottom: Metrics.width * 0.01,
+    },
+    subText: {
+        fontFamily: Fonts.type.light,
+        fontSize: Fonts.size.fourteen,
+        paddingLeft: Metrics.width * 0.038,
+        paddingTop: Metrics.width * 0.03,
+        color: Colors.darkTextColor
+    },
+    bemagineText: {
+        fontFamily: Fonts.type.brand,
+        fontSize: Fonts.size.twenty * 3,
+        color: Colors.brandColor,
+    }
 })
