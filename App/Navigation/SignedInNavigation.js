ef16ca2c4754090452bb017da0ac5618380f4147
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-07-27 01:25:45 +0300

    added test screen for signing out in navigation

--- App/Navigation/SignedInNavigation.js
+++ App/Navigation/SignedInNavigation.js
@@ -2,9 +2,13 @@ import React from "react";
 import { createStackNavigator } from "react-navigation"
 
 import IncompleteScreen from "../Modules/Main/Screens/IncompleteScreen"
+import SignoutTestScreen from "../Modules/Authorization/Screens/SignoutTestScreen";
 
 const SignedInNavigator = createStackNavigator({
     Test: {
+        screen: SignoutTestScreen
+    },
+    Incomplete: {
         screen: IncompleteScreen
     }
 })
