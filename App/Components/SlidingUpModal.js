5ee4c8b18b2ec6f6fdab8e3e6e8c7910ed0b71f5
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-08-07 18:52:43 +0300

    added component SlidingUpModal

---
+++ App/Components/SlidingUpModal.js
@@ -0,0 +1,40 @@
+// Packages
+import React, { Component } from "react"
+
+// RN Components
+import {
+} from "react-native"
+
+// Components
+
+// Actions
+
+// Utils
+
+// Styles
+import getStyles from "./Styles/HeaderStyles"
+import { Fonts, Metrics } from "../StylingConstants"
+import { Colors, Images, themed } from '../Theming'
+
+class SlidingUpModal extends Component {
+
+    // *** LIFECYCLE METHODS *** //
+
+    // *** CALLBACKS *** //
+
+    // *** REF METHODS *** //
+
+    // *** CONVENIENCE METHODS *** //
+
+    // *** EVENT HANDLERS *** //
+
+    // *** RENDER METHODS *** //
+
+    render() {
+        let styles = getStyles(themed.color)
+        return null;
+    }
+
+}
+
+export default SlidingUpModal;
