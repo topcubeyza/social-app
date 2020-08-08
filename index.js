7ea19120d413eaa451a2d39474bde5c55288d88a
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-08-08 19:09:21 +0300

    added maxfontsizemultiplier for whole app

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
  */
 
 import './App/Config/ReactotronConfig'
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text, TextInput} from 'react-native';
 import App from './App/Modules/Main/App';
 import {name as appName} from './app.json';
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.maxFontSizeMultiplier = 1.1;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.maxFontSizeMultiplier = 1.1;
 
 AppRegistry.registerComponent(appName, () => App);
