5c7c9a9e3b706e58cd0071e4ba3936529156dfa2
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-08-08 19:22:58 +0300

    changed app root background color

--- ios/Bemagine/AppDelegate.m
+++ ios/Bemagine/AppDelegate.m
@@ -22,7 +22,7 @@
                                                    moduleName:@"Bemagine"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:51/255 green:51/255 blue:51/255 alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
