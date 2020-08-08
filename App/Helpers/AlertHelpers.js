2973916f157411f1d1854630fab402685dfcbb27
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-08-09 00:48:47 +0300

    documented global helper methods

--- App/Helpers/AlertHelpers.js
+++ App/Helpers/AlertHelpers.js
@@ -1,10 +1,12 @@
 import { store } from "../Modules/Main/App"
 import { AlertActions } from "../Redux/AlertRedux"
 
+// Shortcut function to dispatch an action to add an alert to the alert stack
 export const showAlert = alert => {
   store.dispatch(AlertActions.addAlert(alert));
 }
 
+// Shortcut function to dispatch an action to pop the last alert from the alert stack
 export const closeAlert = () => {
   store.dispatch(AlertActions.removeAlert());
 }
--- App/Helpers/ReduxHelpers.js
+++ App/Helpers/ReduxHelpers.js
@@ -1,12 +1,24 @@
 export const UpdateCauses = {
+    // The update is not relevant to the field of interest
     irrelevant: "irrelevant",
+    // The update is due to a change into fetching state
     fetching: "fetching",
+    // The update is due to failure/error in request
     fail: "fail",
+    // The update is due to successful completion of request
     success: "success"
 }
 
-// returns if component updated because redux props updated
-// if it did, then returns the cause for update
+/**
+ * Returns if component updated because of the update in the relevant field of props.
+ * If it did, then returns the cause for update.
+ * Used for watching changes in redux state.
+ * 
+ * @param {{fetching, error}} prevProps - previous props before update
+ * @param {{fetching, error}} currentProps  - current props after update
+ * @param {String} dataFieldName - the name of the field of props whose value will be sent to isDataFieldSuccess
+ * @param {Function} isDataFieldSuccess - a function that takes data field value as params and returns if the request was successfull
+ */
 export const getUpdateCause = (prevProps, currentProps, dataFieldName, isDataFieldSuccess) => {
     if (prevProps == currentProps) {
         return UpdateCauses.irrelevant
