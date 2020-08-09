3400953dc49450291b402935d2381b61f25f4574
Beyza Topcu <beyza.topcu@bilgeadam.com>
2020-08-09 22:45:34 +0300

    documented redux and utils files of authorization module

--- App/Modules/Authorization/Redux/AuthSagas.js
+++ App/Modules/Authorization/Redux/AuthSagas.js
@@ -3,26 +3,38 @@ import { AuthTypes, AuthActions } from "../Redux/AuthRedux"
 import validate from "validate.js";
 import moment from "moment";
 
-let pauseReloading = false;
-
+// Saga that manages the new auth state
 function* manageChangedAuthState(api, action) {
     try {
         let state = action.payload ? action.payload.state : action.state
+        // get the 'user', it may be the _user property of the state, or it may be the state itself
         let user = (state && state._user) ? state._user : state;
 
+        // If the user is null, then the auth state is signed out
+        // If the user's email is verified, then auth state is signed in
+        // In both cases, put the user in redux state
         if ((user && user.emailVerified) || user == null) {
             yield put(AuthActions.setUser({ user }))
         }
+        // Else, there is a user with unverified email.
+        // Check if this user's displayName is set
+        // Because, when a user is created first but not yet updated with the displayName,
+        // we do not want to go ahead and show a no-name ui.
         else if (user && user.displayName) {
+            // Put the user as a candidate user in Redux state
             yield put(AuthActions.setCandidateUser({ user }))
-            let authState = state;
+
+            // Define the reloaded state
+            let reloadedState;
+            
+            // Reload the user until the email is verified, with 1 second delay between each reload
             while (!user.emailVerified) {
-                user = (authState && authState._user) ? authState._user : authState;
                 let delayed = yield delay(1000, true)
-                if (!pauseReloading) {
-                    authState = yield call(api.reloadUser);
-                }
+                reloadedState = yield call(api.reloadUser);
+                user = (reloadedState && reloadedState._user) ? reloadedState._user : reloadedState;
             }
+
+            // After email is verified, put the user in redux state
             yield put(AuthActions.setUser({ user }))
         }
     } catch (error) {
@@ -39,6 +51,8 @@ function* watchAuthStateChange(api) {
 function* signIn(api, action) {
     try {
         let { email, password } = action.payload;
+
+        // Call the API's sign in method with email and password
         yield call(api.signIn, { email, password });
     } catch (error) {
         if (validate.isString(error)) {
@@ -54,12 +68,25 @@ function* watchSignInRequest(api) {
 function* createUser(api, action) {
     try {
         let { email, password, displayName } = action.payload;
+
+        // Remove the previous candidate user from redux state
         yield put(AuthActions.setCandidateUser({ user: null }));
+
+        // Call the API's createUser method with the email and password
         yield call(api.createUser, { email, password });
+
+        // Update the user with the displayName
         yield call(api.updateUserProfile, { displayName })
+
+        // Send the verification email
         yield call(api.sendVerificationEmail);
+
+        // Reload the user to get a user with the displayName
         let user = yield call(api.reloadUser)
+
+        // Mock authStateChange action, because firebase does not change the auth state upon updating the user displayName
         yield call(manageChangedAuthState, api, { payload: { state: user } })
+
     } catch (error) {
         if (validate.isString(error)) {
             yield put(AuthActions.failure({ error }))
@@ -73,6 +100,7 @@ function* watchCreateUserRequest(api) {
 
 function* signOut(api) {
     try {
+        // Call the API's signout method
         yield call(api.signOut);
     } catch (error) {
         if (validate.isString(error)) {
--- App/Modules/Authorization/Utils/CredentialsCheck.js
+++ App/Modules/Authorization/Utils/CredentialsCheck.js
@@ -3,15 +3,21 @@ import val from "validate.js"
 
 import { Texts, localized } from "../../../Localization";
 
-
+// Email constraint object used by validate.js
 const emailConstraint = {
     from: {
         email: true
     }
 };
 
-export default ({email, password, passwordConfirm, displayName}) => {
+/**
+ * Checks each of the given credential fields.
+ * Returns {ok: true} if fields are valid,
+ *  {ok: false, message: error} if fields are not valid
+ */
+const checkCredentials = ({email, password, passwordConfirm, displayName}) => {
     
+    // If any of the fields are defined/given but are empty strings, it is not valid
     if (
         (email !== undefined && _.isEmpty(email)) || 
         (password !== undefined && _.isEmpty(password)) || 
@@ -23,12 +29,16 @@ export default ({email, password, passwordConfirm, displayName}) => {
             message: localized.text(Texts.errorMessages.fillAllFields)
         };
     }
+
+    // If email field is not a valid email address, it is not valid
     if (val({ from: email }, emailConstraint)) {
         return {
             ok: false,
             message: localized.text(Texts.errorMessages.enterValidEmail)
         };
     }
+
+    // If passwordConfirm is present but does not match password, it is not valid
     if (passwordConfirm !== undefined && password !== passwordConfirm) {
         return {
             ok: false,
@@ -36,7 +46,10 @@ export default ({email, password, passwordConfirm, displayName}) => {
         };
     }
 
+    // If passed all checks, return ok: true
     return {
         ok: true
     };
 }
+
+export default checkCredentials;
